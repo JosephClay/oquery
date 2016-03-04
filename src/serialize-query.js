4cf44c6404c4dcc1e8392c50405ecd1ed4e09d0d
Joseph Clay <jclay@hps.md>
2016-03-04 10:37:48 -0600

    fix issue with serializing Map

--- src/serialize-query.js
+++ src/serialize-query.js
@@ -10,9 +10,7 @@ const format = {
 module.exports = function(map) {
     const obj = {};
     map.forEach((value, key) => {
-        const value = map.get(key);
         obj[key] = (format[key] || general)(value);
-        return obj;
     });
     return obj;
 };
